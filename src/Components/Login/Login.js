ee91cd0a87de34468691ce5ab326a381260a30ae
mdtahidulislam <tahidul.developer@gmail.com>
2021-10-15 16:57:13 +0600

    added login option with

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useFirebase from '../../Hook/usefirebase';
 
 const Login = () => {
+    const { signInUsingGoogle } = useFirebase();
     return (
         <div>
             <h1>Please Login</h1><br />
-            <button>Google Login</button> <br />
+            <button onClick={signInUsingGoogle}>Google Login</button> <br />
             <Link to='/register'>New User?</Link>
         </div>
     );
